f82b389ddb11a0773009643a50c687b2f1371cf5
howtis3621 <howtis3621@gmail.com>
2023-10-26 20:25:32 +0900

    robot.txt 수정 및 custom element 관련 오류 수정

--- vue.config.js
+++ vue.config.js
@@ -8,4 +8,13 @@ module.exports = defineConfig({
       }
     }
   },
+  chainWebpack: config => {
+    config.module
+      .rule('vue').use('vue-loader')
+      .tap(options => ({ ...options,
+        compilerOptions: {
+          isCustomElement: (tag) => tag.startsWith("swiper-")
+        }
+      }))
+  }
 })
